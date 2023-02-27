13f30efd8a5ae114e18a5a4e97e85af05f80f650
Silaev Kirill <s-kir@yandex.ru>
2023-02-27 20:52:54 +0300

    Added initial input of rows in project Final verification

--- task/Final_verification.cs
+++ task/Final_verification.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+
+string[] InputKeywords()
+{
+  Console.Write("Какое количество строк изволите ввести? -> ");
+  int NumberOfRows;
+  string Temp = Console.ReadLine();
+  while (!int.TryParse(Temp, out NumberOfRows) && NumberOfRows <= 0)
+  {
+    Console.Write("Вы ввели не целое и положительное число! Попробуйте еще разок -> ");
+    Temp = Console.ReadLine();
+  }
+  string[] Keywords = new string[NumberOfRows];
+  for (int i = 0; i < NumberOfRows; i++)
+  {
+    Console.Write($"Введите строку {i + 1} -> ");
+    Keywords[i] = Console.ReadLine();
+  }
+  return Keywords;
+}
+
+Console.Clear();
+string[] UserKeywords = InputKeywords();
+
+//Console.Write(UserKeywords);
