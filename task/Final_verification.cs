2b433ea3a96a5e1bd6a4f338869155aecabe556f
Silaev Kirill <s-kir@yandex.ru>
2023-02-28 10:53:10 +0300

    Finished program in project Final verification

--- task/Final_verification.cs
+++ task/Final_verification.cs
@@ -1,11 +1,10 @@
-﻿//
-
+﻿// Ввод строк пользователем с проверкой
 string[] InputKeywords()
 {
   Console.Write("Какое количество строк изволите ввести? -> ");
   int NumberOfRows;
   string Temp = Console.ReadLine();
-  while (!int.TryParse(Temp, out NumberOfRows) && NumberOfRows <= 0)
+  while (!int.TryParse(Temp, out NumberOfRows) || NumberOfRows <= 0)
   {
     Console.Write("Вы ввели не целое и положительное число! Попробуйте еще разок -> ");
     Temp = Console.ReadLine();
@@ -19,7 +18,41 @@ string[] InputKeywords()
   return Keywords;
 }
 
+// Подсчет строк с количеством символов больше равно 3
+int More3Symbol(string[] Arr)
+{
+  int count = 0;
+  for (int i = 0; i < Arr.Length; i++)
+  {
+    if (Arr[i].Length <= 3)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+//Формирование нового массива из строк с количеством символом меньше равно 3
+string[] ProcessAnArray(string[] Arr, int count)
+{
+  string[] ResultArray = new string[count];
+  int j = 0;
+  for (int i = 0; i < Arr.Length; i++)
+  {
+    if (Arr[i].Length <= 3)
+    {
+      ResultArray[j] = Arr[i];
+      j++;
+    }
+  }
+  return ResultArray;
+}
+
 Console.Clear();
 string[] UserKeywords = InputKeywords();
-
-//Console.Write(UserKeywords);
+string[] ProcessedArray = ProcessAnArray(UserKeywords, More3Symbol(UserKeywords));
+// Вывод получившегося массива на экран для проверки
+for (int i = 0; i < ProcessedArray.Length; i++)
+{
+  Console.WriteLine(ProcessedArray[i]);
+}
